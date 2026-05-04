9901aee4eb1787f9f9f188e144989fa1492a9106
Grace Ho <142929246+fatcatnyanko@users.noreply.github.com>
2026-05-04 15:51:00 -0700

    Starter Code Book.java
    added Book attributes, getter methods, setStatus method, toString method

--- Book.java
+++ Book.java
@@ -1,3 +1,74 @@
+/*
+ * Book class that manages the following attributes:
+ 	* Title
+ 	* Author
+ 	* ISBN
+ 	* Publication year
+ 	* Availability status
+ */
+
 public class Book {
-    
+	
+	// Book attributes
+	String title; // title of Book
+	String authorFirst; // author's first name of Book
+	String authorLast; // author's last name of Book
+	String ISBN; // unique # Id of Book
+	int year; // publication year of Book
+	boolean status; // availability status of Book
+	
+	/**
+	 * Create book with certain attributes
+	 * @param title
+	 * @param author
+	 * @param ISBN
+	 * @param year
+	 */
+	public Book(String title, String authorFirst, String authorLast, String ISBN, int year) {
+		this.title = title;
+		this.authorFirst = authorFirst.substring(3);
+		this.authorLast = authorLast;
+		this.ISBN = ISBN;
+		this.year = year;
+		this.status = true; // auto set availability to available
+	}
+	
+	/**
+	 * Change status as needed
+	 * @param status
+	 */
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+	
+	/*
+	 * Getter methods for all attributes
+	 */
+	public String getTitle() {
+		return this.title;
+	}
+	
+	public String getAuthor() {
+		
+		return this.authorLast + "," + this.authorFirst;
+	}
+	
+	public String getISBN() {
+		return this.ISBN;
+	}
+	
+	public int getYear() {
+		return this.year;
+	}
+	
+	public boolean getStatus() {
+		return this.status;
+	}
+	
+	
+	public String toString() {
+		return getTitle() + ", by" + getAuthor() + ", " + getISBN() + 
+				getYear();
+	}
+
 }
